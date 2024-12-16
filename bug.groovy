8d5b14a55eeae49b46016000dae12a7161fd536d
BugHunter-X <askzenai@gmail.com>
2024-12-16 21:40:06 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,15 @@
+```groovy
+class MyClass {
+    def myMethod(arg1, arg2) {
+        if (arg1 == null) {
+            println "arg1 is null"
+            return
+        }
+        println arg1 + arg2
+    }
+}
+
+MyClass myInstance = new MyClass()
+myInstance.myMethod(null, 5)
+myInstance.myMethod([1,2,3], 5)
+```
